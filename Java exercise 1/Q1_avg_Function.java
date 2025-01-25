435027799f69a0aa3c4bee58ae3c95572cd19603
sayan pal <sayansuper330@gmail.com>
2025-01-25 13:11:35 +0530

    add Q2_Sum_Odd_Function

--- Java exercise 1/Q1_avg_Function.java
+++ Java exercise 1/Q1_avg_Function.java
@@ -2,21 +2,22 @@
 
 import java.util.*;
 
-public class Q1_avg_Function{
+public class Q1_avg_Function {
 
     //create avg fuction which doesnt return any value
-    public static void avg(double sum){
-        double avg = sum/3;
-        System.out.println("Average of three numbers is: "+avg);
+    public static void avg(double sum) {
+        double avg = sum / 3;
+        System.out.println("Average of three numbers is: " + avg);
     }
+
     public static void main(String[] args) {
         double sum = 0;
         Scanner sc = new Scanner(System.in);
 
         //user input using for loop and calculate the sum
         System.out.println("Enter Three numbers:");
-        for(int i=1;i<=3;i++){
-            System.out.print("Numbers "+i+": ");
+        for (int i = 1; i <= 3; i++) {
+            System.out.print("Numbers " + i + ": ");
             double num = sc.nextDouble();
             sum += num;
         }
