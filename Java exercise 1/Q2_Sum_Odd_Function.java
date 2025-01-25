7392fa5cffb1732b89d3563f36a843ad401e9f88
sayan pal <sayansuper330@gmail.com>
2025-01-25 13:14:14 +0530

    add Q2_Sum_Odd_Function

---
+++ Java exercise 1/Q2_Sum_Odd_Function.java
@@ -0,0 +1,29 @@
+
+import java.util.Scanner;
+
+public class Q2_Sum_Odd_Function {
+
+    public static void sumOfOdd(int n) {
+        int sum = 0;
+
+        for (int i = 1; i <= n; i++) {
+            if (i % 2 != 0) {
+                sum += i;
+            }
+        }
+        System.out.println("Sum of odd numbers is: " + sum);
+    }
+
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+
+        //take input the range
+        System.out.print("Enter the range: ");
+        int n = sc.nextInt();
+
+        //call sumOfOdd
+        sumOfOdd(n);
+
+        sc.close();
+    }
+}
