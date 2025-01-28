a9117d61b29c5cf94906fd53ce76e8cf4093ba5e
sayan pal <sayansuper330@gmail.com>
2025-01-28 12:29:10 +0530

    update Q7

--- Java exercise 1/Q7_Num_EV.java
+++ Java exercise 1/Q7_Num_EV.java
@@ -1,3 +1,5 @@
+//7.  Write a program to enter the numbers till the user wants and at the end it 
+//should display the count of positive, negative and zeros entered. 
 import java.util.Scanner;
 
 public class Q7_Num_EV {
