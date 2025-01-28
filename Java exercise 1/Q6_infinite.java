64e9c5505fa7505ee40d5197ec864d53ec9980ea
sayan pal <sayansuper330@gmail.com>
2025-01-28 11:31:34 +0530

    add infinite

---
+++ Java exercise 1/Q6_infinite.java
@@ -0,0 +1,10 @@
+//6.Write an infinite loop using do while condition.
+public class Q6_infinite {
+    public static void main(String[] args) {
+        int counter=1;
+        do { 
+            System.out.println("This is the iteration number:"+counter);
+            counter++;
+        } while (true);
+    }
+}
