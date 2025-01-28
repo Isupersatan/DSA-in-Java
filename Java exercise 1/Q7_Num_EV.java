cb2ee08aeadf7068296a3180f8e212118dde38e0
sayan pal <sayansuper330@gmail.com>
2025-01-28 12:17:48 +0530

    add  Q7_Num_Ev

---
+++ Java exercise 1/Q7_Num_EV.java
@@ -0,0 +1,36 @@
+import java.util.Scanner;
+
+public class Q7_Num_EV {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+
+        int posCount = 0;
+        int negCount = 0;
+        int zero = 0;
+
+        System.out.println("Enter Numbers (Enter -999 to stop):");
+
+        while (true) { 
+            System.out.print("Enter a number: ");
+            int num = sc.nextInt();
+
+            if (num == -999) {
+                break;
+            }
+
+            if (num>0) {
+                posCount++;
+            } else if(num<0) {
+                negCount++;
+            }else{
+                zero++;
+            }
+        }
+        System.out.println("\nResult:- ");
+        System.out.println("Positive numbers: " + posCount);
+        System.out.println("Negative numbers: " + negCount);
+        System.out.println("Zero: " + zero);
+
+        sc.close();
+    }    
+}
