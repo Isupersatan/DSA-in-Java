846436899df3e57c3bdbf2d44f1d176edbbac269
sayan pal <sayansuper330@gmail.com>
2025-02-06 21:44:03 +0530

    add arrString

---
+++ Strings/arrString.java
@@ -0,0 +1,22 @@
+//Take an array of Strings input from the user & find the cumulative (combined) length of all those strings.
+
+import java.util.Scanner;
+
+public class arrString{
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        
+        System.out.print("Enter the size of array of String: ");
+        int size = sc.nextInt();
+
+        int totLength = 0;
+
+        String array[] = new String[size];
+        System.out.println("Enetr the element of array:");
+        for(int i=0;i<size;i++){
+            array[i] = sc.next();
+            totLength += array[i].length();
+        }
+        System.out.println("Cumulative length of all strings is "+totLength);
+    }
+}
